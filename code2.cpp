f93331ffa0e87782da8129b129568bc887b6a5c2
sylviakaguora <sylivia.kaguora@strathmore.edu>
2016-04-19 18:04:45 +0300

    Added files via upload

---
+++ code2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    for(N=0; N<5;N++)
+    {
+        
+       cout << "Hello, World\n";
+    }
+    return 0;
+}
